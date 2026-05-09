f8d51dc48c1ad457609857dc3734dbc3e2c51373
holmgard9898 <gustav.holmgard@gmail.com>
2026-05-09 17:54:16 +0200

    Update MainMenu.tsx

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -22,7 +22,7 @@ const MainMenu = () => {
       style={{ ...bg.style, backgroundPosition: 'center 55%', backgroundSize: 'cover' }}
     >
       {/* Här har jag ändrat mt-[22vh] till mt-[30vh] för att sänka loggan */}
-      <div className="w-full max-w-[16rem] sm:max-w-[18rem] flex justify-center mt-[30vh] mb-[3vh]">
+      <div className="w-full max-w-[16rem] sm:max-w-[18rem] flex justify-center mt-[40vh] mb-[3vh]">
         <img
           src={logoUrl}
           alt="Word Rumble"
