1c498f689413f71425dfd67a699d9ec495a40be8
holmgard9898 <gustav.holmgard@gmail.com>
2026-05-09 18:21:55 +0200

    Update SingleplayerMenu.tsx

--- src/pages/SingleplayerMenu.tsx
+++ src/pages/SingleplayerMenu.tsx
@@ -139,7 +139,7 @@ const SingleplayerMenu = () => {
         </div>
 
         <div className="transition-all duration-300 overflow-hidden" style={{ maxHeight: expanded ? 0 : 60, opacity: expanded ? 0 : 1 }}>
-          <BackButton onClick={() => go('/', 'classic')} icon={<ArrowLeft className="w-4 h-4" />} label={t.mainMenu} className="mt-4" />
+          <BackButton onClick={() => go('/', 'classic')} icon={<ArrowLeft className="w-4 h-4" />} label={t.mainMenu} className="mt-1" />
         </div>
       </div>
 
